2c4db49b93aceb0cbbca34f128db78a2cb0f7c6d
桓一 <41766129+huanby@users.noreply.github.com>
2020-08-21 12:20:40 +0800

    修改专业数据填报模块 填报功能完成部分

---
+++ src/main/java/net/webset/mapper/ImagesMajorMapper.java
@@ -0,0 +1,13 @@
+package net.webset.mapper;
+
+import net.webset.entity.ImagesMajor;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * 专业所属附件Mapper类
+ * @author hby
+ * create date: 2020-08-18
+ */
+public interface ImagesMajorMapper extends BaseMapper<ImagesMajor> {
+
+}
